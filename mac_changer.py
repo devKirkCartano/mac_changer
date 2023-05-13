a1c86abbd46c2795c934c6eabe8baed605b89dd6
Kirk Cedrick Cartaño <kc.cartano724@gmail.com>
2023-05-13 11:10:51 -0400

    added a mac changer function

--- mac_changer.py
+++ mac_changer.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python
+
 import subprocess
 import optparse
+
+
+def change_mac(interface, new_mac):
+    print("[+] Changing MAC Address to", new_mac)
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
+    subprocess.call(["ifconfig"])
+
 
 parser = optparse.OptionParser()  # created an object for parsing
 parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
 parser.add_option("-m", "--mac", dest="new_mac", help="New MAC Address")
-
 (options, arguments) = parser.parse_args()  # created a variable for arguments and options
 
-interface = options.interface
-new_mac = options.new_mac
-
+change_mac(options.interface, options.new_mac)
 # #!/usr/bin/env python3 version of code
 # import subprocess
 # import argparse
@@ -23,9 +30,3 @@ new_mac = options.new_mac
 #
 # interface = args.interface
 # new_mac = args.new_mac
-
-print("[+] Changing MAC Address to", new_mac)
-subprocess.call(["ifconfig", interface, "down"])
-subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
-subprocess.call(["ifconfig", interface, "up"])
-subprocess.call(["ifconfig"])
