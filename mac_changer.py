c976feb775bf66ebdbc85b4a0f5b6c28f7941d6b
Kirk Cedrick Cartaño <kc.cartano724@gmail.com>
2023-05-14 01:59:36 -0400

    added variable for checking output

--- mac_changer.py
+++ mac_changer.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 
 import subprocess
 import optparse
@@ -21,22 +21,10 @@ def change_mac(interface, new_mac):
     subprocess.call(["ifconfig", interface, "down"])
     subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
     subprocess.call(["ifconfig", interface, "up"])
-    subprocess.call(["ifconfig"])
 
 
 options = get_arguments()  # created a variable for arguments and options
-change_mac(options.interface, options.new_mac)
+# change_mac(options.interface, options.new_mac)
 
-
-# #!/usr/bin/env python3 version of code
-# import subprocess
-# import argparse
-#
-# parser = argparse.ArgumentParser()
-# parser.add_argument("-i", "--interface", dest="interface", help="Interface to change its MAC address")
-# parser.add_argument("-m", "--mac", dest="new_mac", help="New MAC Address")
-#
-# args = parser.parse_args()
-#
-# interface = args.interface
-# new_mac = args.new_mac
+ifconfig_result = subprocess.check_output(["ifconfig", options.interface])
+print(ifconfig_result)
